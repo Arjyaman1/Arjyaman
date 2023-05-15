4162fb1fd79426fb9ffa72f5ce51cd88b51d01b0
ARJYAMAN CHOUDHURY <117749612+Arjyaman1@users.noreply.github.com>
2023-05-15 21:35:01 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -518,7 +518,7 @@ function arlo_tm_animate_text() {
   var animateSpan = jQuery(".arlo_tm_animation_text_word");
 
   animateSpan.typed({
-    strings: ["FrontEnd Developer", "Designer and developer", "Coder", "Freelancer"],
+    strings: ["FrontEnd Developer", "Designer", "developer"],
     loop: true,
     startDelay: 1e3,
     backDelay: 2e3,
